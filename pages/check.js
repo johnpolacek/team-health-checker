f0f1c8ab5ee4311915775b5d677ddc4f89855c71
johnpolacek <johnpolacek@gmail.com>
2019-03-01 07:03:58 -0600

    add getHealthCheckQuery to check page

--- pages/check.js
+++ pages/check.js
@@ -3,14 +3,24 @@ import App from '../components/App'
 import { Query } from 'react-apollo'
 import gql from 'graphql-tag'
 
+const getHealthCheckQuery = gql`query HealthCheck($id: ID!) {
+  HealthCheck(id: $id) {
+    id
+    responses {
+      id
+      ratings
+    }
+  }
+}`
+
 const HealthCheck = ({ id }) => (
   <App>
-      <Query query={getHealthCheckQuery} variables={{id: this.props.id}}>
+      <Query query={getHealthCheckQuery} variables={{id}}>
         {({ loading, error, data }) => {
           if (loading) return <div>Loading...</div>
           if (error || !data.HealthCheck) return <div>Error: Could not load HealthCheck with id: {this.props.id}</div>
           return (
-            <h1>Loaded HealthCheck id: {this.props.id}</h1>
+            <h1>Loaded HealthCheck id: {id}</h1>
           )
         }}
       </Query>
