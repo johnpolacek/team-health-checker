0f047007e4ec7e64c05949476d6623366a36f3be
johnpolacek <johnpolacek@gmail.com>
2019-03-01 05:30:50 -0600

    set up test page for /check/:id route

--- pages/check.js
+++ pages/check.js
@@ -1,14 +1,11 @@
-import React from 'react'
 import App from '../components/App'
 
-export default class extends React.Component {
-  static getInitialProps ({ query: { id } }) {
-    return { id }
-  }
-
-  render () {
-    return <App>
-      <h1>Loaded HealthCheck id: {this.props.id}</h1>
-    </App>
-  }
+const HealthCheck = ({ id }) => (
+  <App>
+    <h1>Loaded HealthCheck id: {id}</h1>
+  </App>
+)
+HealthCheck.getInitialProps = async ({ query }) => {
+  return { id: query.id }
 }
+export default HealthCheck
