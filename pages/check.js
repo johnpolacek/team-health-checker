43f47aea8ecefb59d7d4b6041d2fd3e227e4bf1f
johnpolacek <johnpolacek@gmail.com>
2019-03-04 17:23:54 -0600

    fix getHealthCheckQuery import

--- pages/check.js
+++ pages/check.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import App from '../components/App'
-import getHealthCheckQuery from '../api/operations'
+import { getHealthCheckQuery } from '../api/operations'
 import { Query } from 'react-apollo'
 
 const HealthCheck = ({ id }) => (
